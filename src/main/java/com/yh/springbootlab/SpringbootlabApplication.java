ef1c34f621f8e36fe0df8955cc6f6e976d561816
杨槐 <2774584057@qq.com>
2020-01-11 20:30:35 +0800

    初始化

---
+++ src/main/java/com/yh/springbootlab/SpringbootlabApplication.java
@@ -0,0 +1,13 @@
+package com.yh.springbootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yh/springbootlab/SpringbootlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.springbootlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
