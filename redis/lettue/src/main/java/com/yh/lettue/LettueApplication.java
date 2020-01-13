47637e3f4018769fb8d3e016884dd73dc9955762
huaiyang <123456789qweyh>
2020-01-13 18:32:45 +0800

    增加redisCache配置

--- redis/lettue/src/main/java/com/yh/lettue/LettueApplication.java
+++ redis/lettue/src/main/java/com/yh/lettue/LettueApplication.java
@@ -3,6 +3,7 @@ package com.yh.lettue;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.yh.lettue.repo") //扫描的mapper
