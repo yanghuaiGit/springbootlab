c4c2dfe42909eb32abb2a27ba3214c0325c197d5
huaiyang <123456789qweyh>
2020-01-13 09:09:44 +0800

    增加redis配置

---
+++ redis/jedis/src/main/java/com/yh/jedis/config/JedisConfig.java
@@ -0,0 +1,10 @@
+package com.yh.jedis.config;
+
+/**
+ * @author huaiyang
+ * @version 1.0.0
+ * @date 2020/1/13
+ * @copyright 本内容仅限于浙江云贸科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public class JedisConfig {
+}
