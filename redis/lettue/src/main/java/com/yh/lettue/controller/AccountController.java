469a36fe2451fdd28f577d65cf817cefd1546c1d
huaiyang <123456789qweyh>
2020-01-15 15:20:28 +0800

    增加redis zset取交集操作

--- redis/lettue/src/main/java/com/yh/lettue/controller/AccountController.java
+++ redis/lettue/src/main/java/com/yh/lettue/controller/AccountController.java
@@ -3,11 +3,6 @@ package com.yh.lettue.controller;
 import com.yh.lettue.model.pojo.AccountUser;
 import com.yh.lettue.model.vo.AccountCacheUser;
 import com.yh.lettue.service.AccountUserService;
-import org.springframework.data.redis.connection.RedisStringCommands;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.types.Expiration;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -24,12 +19,6 @@ public class AccountController {
     @Resource
     private AccountUserService accountUserService;
 
-    @Resource
-    private RedisTemplate<String, AccountCacheUser> redisTemplate;
-
-    @Resource
-    private Jackson2JsonRedisSerializer jackson2JsonRedisSerializer;
-
 
     @PostMapping
     public void createAccount(@RequestBody AccountUser accountUser) {
@@ -45,21 +34,5 @@ public class AccountController {
     public List<AccountCacheUser> queryByName(@RequestParam String name) {
         return accountUserService.queryByName(name);
     }
-
-
-    @GetMapping("/test")
-    public void test() {
-        AccountCacheUser build = AccountCacheUser.builder().id(2L).username("redis").type(1).typeDesc("desc").build();
-        redisTemplate.opsForValue().set("1", AccountCacheUser.builder().id(1L).username("redis").type(1).typeDesc("desc").build());
-        redisTemplate.execute((RedisCallback<Boolean>) redisConnection ->
-                redisConnection.set("2".getBytes(), jackson2JsonRedisSerializer.serialize(build), Expiration.seconds(5000),
-                        RedisStringCommands.SetOption.SET_IF_ABSENT));
-    }
-
-    @GetMapping("/get")
-    public AccountCacheUser get() {
-        return redisTemplate.opsForValue().get("1");
-    }
-
 
 }
--- redis/lettue/src/test/java/com/yh/lettue/LettueApplicationTests.java
+++ redis/lettue/src/test/java/com/yh/lettue/LettueApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 @Slf4j
@@ -42,27 +43,50 @@ class LettueApplicationTests {
 
     @Test
     void get() {
-        System.out.println(redisTemplate.opsForValue().get("1"));
+
+        //获取不存在的key 获取的值为null
+        System.out.println(redisTemplate.opsForValue().get("123444"));
     }
 
     @Test
     void redisOpt() {
-        stringStringRedisTemplate.opsForZSet().add("12","123",123);
-        stringStringRedisTemplate.opsForZSet().add("12","1223",123);
+        stringStringRedisTemplate.opsForZSet().add("12", "123", 123);
+        stringStringRedisTemplate.opsForZSet().add("12", "1223", 123);
 
-        stringStringRedisTemplate.opsForZSet().add("122","12233",123);
-        stringStringRedisTemplate.opsForZSet().add("122","1223",1223);
+        stringStringRedisTemplate.opsForZSet().add("122", "12233", 123);
+        stringStringRedisTemplate.opsForZSet().add("122", "1223", 1223);
 
         //将12 和 122两个集合取交集 其分数值为2个集合对应的分数值之和
-        stringStringRedisTemplate.opsForZSet().intersectAndStore("12","122","2");
+        stringStringRedisTemplate.opsForZSet().intersectAndStore("12", "122", "2");
 
-        stringStringRedisTemplate.opsForValue().set("ueser:123:2344","233");
-        stringStringRedisTemplate.opsForValue().set("ueser2::123:2344","233");
-
+        stringStringRedisTemplate.opsForValue().set("ueser:123:2344", "233");
+        stringStringRedisTemplate.opsForValue().set("ueser2::123:2344", "233");
 
 
         log.info("是否存在{}", redisManager.hset("user1", "name", "yanghhuai"));
         log.info("是否存在{}", redisManager.hset("user1", "sex", "1"));
         log.info("是否存在{}", redisManager.hset("user2", "name", "wuyan"));
+    }
+
+    @Test
+    void incre() {
+        log.info("对不存在的值进行自增{}", stringStringRedisTemplate.opsForValue().increment("incre"));
+        log.info("对不存在的值进行自增{}", stringStringRedisTemplate.opsForValue().increment("incre1", 22));
+
+        log.info("对不存在的值进行自减{}", stringStringRedisTemplate.opsForValue().decrement("incre2"));
+        log.info("对不存在的值进行自减{}", stringStringRedisTemplate.opsForValue().decrement("incre22", 22));
+    }
+
+    @Test
+    void expirOpt() {
+        //key不存在 返回的值是false 不会设值  只有key存在 才会进行更新
+        Boolean s1 = stringStringRedisTemplate.opsForValue().setIfPresent("s1", "2");
+        log.info("对不存在的key进行 setIfPresent{}", s1);
+
+        stringStringRedisTemplate.opsForValue().set("time", "1", 100L, TimeUnit.MINUTES);
+
+        //会将过期时间重置为-1
+        s1 = stringStringRedisTemplate.opsForValue().setIfPresent("time", "2");
+        log.info("对已存在的key进行 setIfPresent{}", s1);
     }
 }
