d91eabc8dc43c49d0ecdd58d8e082826e9da0480
huaiyang <123456789qweyh>
2020-01-21 14:02:04 +0800

    增加springBeans模块

---
+++ spring-beans/src/main/java/com/yh/springbeans/SpringBeansApplication.java
@@ -0,0 +1,13 @@
+package com.yh.springbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeansApplication.class, args);
+    }
+
+}
---
+++ spring-beans/src/test/java/com/yh/springbeans/SpringBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.springbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
